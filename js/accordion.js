18d4c521b08f9e6ce14d4482bf105286b4773de8
Your Name <you@example.com>
2023-04-25 23:35:40 +0300

    написал аккордеон

--- js/accordion.js
+++ js/accordion.js
@@ -1,6 +1,19 @@
 const buttonFeatureLink = document.querySelector('.feature__link');
 const buttonFeatureLinks = document.querySelectorAll('.feature__link'); 
+const listsButton = document.querySelectorAll('.feature-sub'); 
 
-buttonFeatureLink.addEventListener ('click', () => {
-  console.log('Кнопка найдена');
-});
+buttonFeatureLinks.forEach((item, index ) => {
+  item.addEventListener ('click', () => {
+    buttonFeatureLinks.forEach((item ) => {
+      item.classList.remove ('feature__link_active')
+    });
+
+    item.classList.toggle ('feature__link_active');
+
+    listsButton.forEach((listItem ) => {
+      listItem.classList.add ('hidden')
+    })
+
+    listsButton[index].classList.remove('hidden');
+  })
+})
