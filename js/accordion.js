b2c533269da5c74d2107078b6d4e531587d1b0f5
Your Name <you@example.com>
2023-04-25 19:14:15 +0300

    пробник

---
+++ js/accordion.js
@@ -0,0 +1,6 @@
+const buttonFeatureLink = document.querySelector('.feature__link');
+const buttonFeatureLinks = document.querySelectorAll('.feature__link'); 
+
+buttonFeatureLink.addEventListener ('click', () => {
+  console.log('Кнопка найдена');
+});
