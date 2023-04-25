edc8d3e335812ed3596c51336c3a8c645cda2b95
Your Name <you@example.com>
2023-04-26 00:08:01 +0300

    рефакторинг

--- js/accordion.js
+++ js/accordion.js
@@ -1,19 +1,8 @@
-const buttonFeatureLink = document.querySelector('.feature__link');
-const buttonFeatureLinks = document.querySelectorAll('.feature__link'); 
 const listsButton = document.querySelectorAll('.feature-sub'); 
 
-buttonFeatureLinks.forEach((item, index ) => {
-  item.addEventListener ('click', () => {
-    buttonFeatureLinks.forEach((item ) => {
-      item.classList.remove ('feature__link_active')
-    });
-
-    item.classList.toggle ('feature__link_active');
-
-    listsButton.forEach((listItem ) => {
-      listItem.classList.add ('hidden')
-    })
-
-    listsButton[index].classList.remove('hidden');
+buttonFeatureLinks.forEach((item, index) => {
+  item.addEventListener('click', () => {
+    item.classList.toggle('feature__link_active')
+    listsButton[index].classList.toggle('hidden')
   })
 })
