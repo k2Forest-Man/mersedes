6b330b231e64038ca3959998e16ecb524fad1b5c
Your Name <you@example.com>
2023-04-27 16:54:16 +0300

    добавил коммент

--- js/tabs.js
+++ js/tabs.js
@@ -2,10 +2,12 @@ const tabsHandlerElements = document.querySelectorAll('[data-tabs-handler]');
 const tabsContentElements = document.querySelectorAll('[data-tabs-field]');
 
 for (let button of tabsHandlerElements) {
+  //Навешиваем через клик актвный таб
   button.addEventListener ('click', () => {
     tabsHandlerElements.forEach (item => item.classList.remove('design-list__item_active'));
     button.classList.add('design-list__item_active');
 
+    //Переключение табов описания блоков и фото
     tabsContentElements.forEach (itemCont => {
       if (itemCont.dataset.tabsField === button.dataset.tabsHandler) {
         itemCont.classList.remove('hidden');
