9fa0d825e66db2c033b0f8396904b6ab5a1d9d3d
Your Name <you@example.com>
2023-04-27 16:48:10 +0300

    реализовал смену табов при клике мышкой

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,18 @@
-console.log('i am work');
+const tabsHandlerElements = document.querySelectorAll('[data-tabs-handler]');
+const tabsContentElements = document.querySelectorAll('[data-tabs-field]');
+
+for (let button of tabsHandlerElements) {
+  button.addEventListener ('click', () => {
+    tabsHandlerElements.forEach (item => item.classList.remove('design-list__item_active'));
+    button.classList.add('design-list__item_active');
+
+    tabsContentElements.forEach (itemCont => {
+      if (itemCont.dataset.tabsField === button.dataset.tabsHandler) {
+        itemCont.classList.remove('hidden');
+      } else {
+        itemCont.classList.add('hidden');
+      }
+    })
+  })
+};
+
