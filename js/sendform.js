c171f65547568e631fe39773cb74485f0d8053f1
k2Forest-Man <LesnoiChelovek@yandex.ru>
2023-04-28 23:17:14 +0300

    закончил валидацию формы

---
+++ js/sendform.js
@@ -0,0 +1,34 @@
+const mainForm = document.querySelector('.form-test-drive');
+
+mainForm.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+
+  let objectData = {};
+
+  for (let { name, value } of mainForm.elements) {
+
+    if (name) {
+      objectData[name] = value
+    }
+  }
+
+  //Отправляем данные формы на сервер
+  fetch('https://jsonplaceholder.typicode.com/posts', { 
+    method: 'POST',
+    body: JSON.stringify(objectData)
+  })
+    .then(response => {
+      if (response.status === 200 || response.status === 201) {
+        return response.json()
+      } else {
+        throw new Error(response.status)
+      }
+    })
+    .then(objectData => {
+      alert('Данные успешно сохранены!')
+      mainForm.reset()
+    })
+    .catch(error => {
+      alert('Произошла ошибка, статус ' + error.message)
+    })
+});
